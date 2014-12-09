951e3b911b384bff1c05fd0caf94f58762e691ed
Max Terry <mhterry@gmail.com>
2014-12-09 18:30:02 -0500

    Finish scraper

--- scripts/usdetour.js
+++ scripts/usdetour.js
@@ -1,10 +1,6 @@
-USDetour = {
-	departments: {},
-	aisles: {},
-	products: {},
-	producers: {},
-	merchants: {}
-}
+USDetour = {}
+
+USDetour.products = []
 
 USDetour.getElementName = function(elt) {
 	var name = (elt.children.length? elt.children[0] : elt).textContent.trim()
@@ -13,61 +9,64 @@ USDetour.getElementName = function(elt) {
 
 USDetour.getElementId = function(elt) {
 	return USDetour.getElementName(elt)
-		.replace(/\s/g, '_')
+		.replace(/\s/g, '-')
 		.replace(/&/g, 'and')
 		.toLowerCase()
 }
 
-USDetour.parse = function() {
+USDetour.scrape = function() {
 	var elts = document.body.children
 	var current = {
-		department: null,
-		aisle: null,
 		product: null,
 		option: null
 	}
 	for (var i=0, l=elts.length; i<l; i++) {
 		var elt = elts[i]
 		if (elt.tagName == 'H2') {
-			current.department = USDetour.getElementId(elt)
-			current.aisle = null
-			USDetour.departments[current.department] = {
+			current.department = {
+				id: elt.id,
 				name: USDetour.getElementName(elt)
 			}
+			current.aisle = null
 		}
 		else if (elt.tagName == 'H3') {
-			current.aisle = USDetour.getElementId(elt)
-			USDetour.aisles[current.aisle] = {
-				name: USDetour.getElementName(elt),
-				department: current.department
+			current.aisle = {
+				id: elt.id,
+				name: USDetour.getElementName(elt)
 			}
 		}
 		else if (elt.tagName == 'H4') {
-			current.product = USDetour.getElementId(elt)
-			USDetour.products[current.product] = {
+			current.product = {
 				department: current.department,
 				aisle: current.aisle,
-				name: USDetour.getElementName(elt)
+				id: elt.id,
+				name: USDetour.getElementName(elt),
+				producer: {},
+				description: [],
+				origin: {},
+				discovered: {},
+				options: []
 			}
+			USDetour.products.push(current.product)
 		}
 		else if (elt.tagName == 'H5') {
-			var name = USDetour.getElementName(elt)
-			current.producer = USDetour.getElementId(elt)
-			USDetour.producers[current.producer] = {
-				name: name
+			current.product.producer = {
+				id: USDetour.getElementId(elt),
+				name: USDetour.getElementName(elt),
+				url: elt.children[0].href
 			}
-			USDetour.products[current.product].producer = USDetour.getElementId(elt)
 		}
 		else if (elt.tagName == 'P') {
 			if (elt.className == 'description') {
-				USDetour.products[current.product].description = elt.innerText.substr(0, elt.innerText.length-1).trim().split('. ')
+				var desc = elt.innerHTML.substr(0, elt.innerHTML.length-1).trim().split('\n\t\t')
+				current.product.description = desc
 			}
 			else if (elt.className == 'origin') {
 				var local = null
 				if (elt.children[0].children[0]) {
 					var local = elt.children[0].children[0].title.split(',')[0]
 				}
-				USDetour.products[current.product].origin = {
+				current.product.origin = {
 					state: elt.innerText.split("Made in ")[1],
 					local: local
 				}
@@ -75,7 +74,7 @@ USDetour.parse = function() {
 			else if (elt.className == 'discovered') {
 				var url = elt.children[0].href.split('/')
 				var date = elt.innerText.split("in ")[1]
-				USDetour.products[current.product].discovered = {
+				current.product.discovered = {
 					person: elt.children[0].innerText,
 					address: url[url.length-1],
 					date: date.substr(0, date.indexOf('.') > -1? date.indexOf('.') : date.indexOf('?'))
@@ -95,24 +94,47 @@ USDetour.parse = function() {
 		}
 		else if (elt.tagName == 'IMG') {
 			var url = elt.src.split('/')
-			current.option.media.push(url[url.length-1])
+			url = url[url.length-1]
+			current.option.media.push(url)
 		}
 		else if (elt.tagName == 'VAR') {
-			var merchant = {}
+			for (var j=0, len=elt.children.length; j<len; j++) {
+				var merchant = { id: '', name: '', url: '', price: 0, logo: null }
+				if (elt.children[j].children[1]) {
+					merchant.id = elt.children[j].className
+					merchant.name = elt.children[j].children[1].children[0].alt
+					var logoUrl = elt.children[j].children[1].children[0].src.split('/')
+					logoUrl = logoUrl[logoUrl.length-1]
+					merchant.logo = logoUrl
+				}
+				else {
+					merchant.id = current.product.producer.id
+					merchant.name = current.product.producer.name
+				}
+				merchant.url = elt.children[j].href
+				merchant.price = elt.children[j].innerText.substr(1).split(" via")[0].split('.').join('') * 1
+				current.option.merchants.push(merchant)
+			}
 		}
-		else if (elt.tagname == 'HR') {
-			USDetour.products[current.product].options.push(current.option)
-			current.product = null
+		else if (elt.tagName == 'HR') {
+			current.product.options.push(current.option)
 		}
 	}
 }
 
 USDetour.show = function(x) {
-	USDetour.parse()
+	USDetour.scrape()
 	return JSON.stringify(USDetour[x], null, 2)
 }
 
 USDetour.count = function(x) {
-	USDetour.parse()
+	USDetour.scrape()
 	return Object.keys(USDetour[x]).length
+}
+
+USDetour.render = function() {
+	for (var i=0, l=USDetour.products.length; i<l; i++) {
+		var product = USDetour.products[i]
+		// TODO
+	}
 }
