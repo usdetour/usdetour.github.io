8d46c0f2dc1d38eb2f6e70398b987f579475a382
Max Terry <mhterry@gmail.com>
2015-01-07 16:57:19 -0500

    Add stubs to dining and start responsive menu display

---
+++ pages/script.js
@@ -0,0 +1,23 @@
+// document.getElementById('logo').onClick = function() {
+// 	var x = document.getElementById('menu').children.getElementsByTagName('a')
+// 	console.log(x)
+// }
+
+var shown = false
+
+function toggleMenu(elt) {
+	if (shown) {
+		var links = document.getElementById('menu').children[0].getElementsByTagName('a')
+		for (var i=1, l=links.length; i<l; i++) {
+			if (links[i].className != 'top') links[i].style.display = 'none'
+		}
+		shown = false
+	}
+	else {
+		var links = document.getElementById('menu').children[0].getElementsByTagName('a')
+		for (var i=1, l=links.length; i<l; i++) {
+			links[i].style.display = 'block'
+		}
+		shown = true	
+	}
+}
