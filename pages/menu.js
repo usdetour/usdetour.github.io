c28b177f18895e1a3bc51b0a0269dfa514298ace
Max Terry <mhterry@gmail.com>
2015-01-08 22:12:12 -0500

    Finish responsive menu

--- pages/menu.js
+++ pages/menu.js
@@ -7,14 +7,14 @@ var shown = false
 
 function toggleMenu(elt) {
 	if (shown) {
-		var links = document.getElementById('menu').children[0].getElementsByTagName('a')
+		var links = document.getElementById('top').children[0].getElementsByTagName('a')
 		for (var i=1, l=links.length; i<l; i++) {
-			if (links[i].className != 'top') links[i].style.display = 'none'
+			if (links[i].className != 'toggle') links[i].style.display = 'none'
 		}
 		shown = false
 	}
 	else {
-		var links = document.getElementById('menu').children[0].getElementsByTagName('a')
+		var links = document.getElementById('top').children[0].getElementsByTagName('a')
 		for (var i=1, l=links.length; i<l; i++) {
 			links[i].style.display = 'block'
 		}
