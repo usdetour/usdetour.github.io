6b2e4873b40c95c6f80c9db3cee7805a87e71d87
Max Terry <mhterry@gmail.com>
2015-01-27 19:47:34 -0500

    Add Firefox support for location toggle

--- pages/menu.js
+++ pages/menu.js
@@ -2,16 +2,16 @@ function toggleLocations() {
 	var states = document.getElementsByClassName('state')
 	for (var i=0, l=states.length; i<l; i++) {
 		states[i].onclick = function() {
-			var localities = this.getElementsByTagName('b')
-			for (var j=0, lj=localities.length; j<lj; j++) {
-				var locale = localities[j]
+			var locales = this.getElementsByTagName('b')
+			for (var j=0, lj=locales.length; j<lj; j++) {
+				var locale = locales[j]
 				if (locale.title) {
-					if (locale.innerText == locale.title) {
-						locale.innerText = locale.name
+					if (locale.innerHTML == locale.title) {
+						locale.innerHTML = locale.name
 					}
 					else {
-						locale.name = locale.innerText
-						locale.innerText = locale.title
+						locale.name = locale.innerHTML
+						locale.innerHTML = locale.title
 					}
 				}
 			}
