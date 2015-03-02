754d9adcff9b6470bce1d8efe465c7d1b798cbc1
Max Terry <mhterry@gmail.com>
2015-03-01 21:54:53 -0500

    Revise menu

--- scripts/usdetour.js
+++ scripts/usdetour.js
@@ -19,26 +19,6 @@ function toggleLocations() {
 	}
 }
 
-var shownMenu = false
-
-function toggleMenu(elt) {
-	if (shownMenu) {
-		var links = document.getElementById('top').children[0].getElementsByTagName('a')
-		for (var i=1, l=links.length; i<l; i++) {
-			if (links[i].className != 'toggle') links[i].style.display = 'none'
-		}
-		shownMenu = false
-	}
-	else {
-		var links = document.getElementById('top').children[0].getElementsByTagName('a')
-		for (var i=1, l=links.length; i<l; i++) {
-			links[i].style.display = 'block'
-		}
-		shownMenu = true
-	}
-	return false
-}
-
-document.body.onload = function() {
+window.onload = function() {
 	toggleLocations()
 }
